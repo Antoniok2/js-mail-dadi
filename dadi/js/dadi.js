2d9c40844abc0008a28c9f95cb1a3cb2237021ae
Antoniok2 <antoniopiazza18@gmail.com>
2021-10-26 15:41:35 +0200

    refactoring js dadi

--- dadi/js/dadi.js
+++ dadi/js/dadi.js
@@ -4,16 +4,16 @@
 // GENERO UN NUMERO RANDOM DA 1 A 6 PER L'USER e PER IL PC
 let userNumber = Math.floor(Math.random() * 6) + 1;
 let pcNumber = Math.floor(Math.random() * 6) + 1;
-let risultatoDado = "Parità"
+let risultatoDado = "Parità";
 
 console.log(userNumber);
 console.log(pcNumber);
 
 // CONDIZIONI SVILUPPO ESERCIZIO
 if (userNumber > pcNumber) {
-    risultatoDado = "Hai vinto tu!"
+    risultatoDado = alert("Complimenti, hai vinto TU!");
 }else if (userNumber < pcNumber) {
-    risultatoDado = "Il pc ha perso!"
-}
+    risultatoDado = alert("Mi dispiace, il PC ha vinto!");
+};
 
 console.log(risultatoDado);
